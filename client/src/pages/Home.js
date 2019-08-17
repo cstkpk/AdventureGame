02f3ad6b374f5515c8c268d11eda6bda23f5911f
Cara Sunberg <cnsunberg@gmail.com>
2019-08-17 14:17:31 -0400

    Basic React router functionality in place for test Home and Results pages

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+function Home() {
+    return(
+        <div>
+            <p>Testing 1 2 3!</p>
+        </div>
+    );
+};
+
+export default Home;
---
+++ client/src/pages/Results.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+function Results() {
+    return(
+        <div>
+            <p>This might be a results page?</p>
+        </div>
+    );
+};
+
+export default Results;
