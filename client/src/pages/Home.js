9ae34b52653c9ef9249f72724dca8f4eeb04a9da
Cara Sunberg <cnsunberg@gmail.com>
2019-08-17 14:47:39 -0400

    Starting to play with data array and displaying quesitons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
+import QA from "../data/questions.json";
 
-function Home() {
-    return(
-        <div>
-            <p>Testing 1 2 3!</p>
-        </div>
-    );
+class Home extends Component {
+
+    state = {
+        QA,
+        question: QA[0].question,
+        answersChoices: [],
+        chosen: ""
+    };
+
+    chooseQuestion = () => {
+        let QA = [...this.state.QA];
+        this.setState({ question: QA[0].question })
+    }
+    
+    render() {
+        return(
+            <div>
+                <p>Testing 1 2 3!</p>
+                <p>Question one: {this.state.question}</p>
+            </div>
+        );
+    }
+
 };
 
 export default Home;
