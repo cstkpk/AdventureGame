aa8edbefe17bfa806c4bd6a34caeb1b8647b366a
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-08-21 13:40:19 -0400

    Added requirement statements for gifs and images in index.js

--- client/src/components/Animation/assets/index.js
+++ client/src/components/Animation/assets/index.js
@@ -1,10 +1,21 @@
 module.exports = {
-    rocket1: require('./rocket1.gif'),
-    rocketLeaveEarth: require("./gifs/Ship_TakeOff.gif"),
-    // rocketExplode: require("./gifs/Ship_4_Explode.gif"),
-    // interiorPowerOn: require("./gifs/Ship_4_interior_power_on.gif"),
-    // moonLanding: require("./gifs/Ship_4_interior_moon_landing.gif")
-}
+  rocket1: require("./rocket1.gif"),
+  rocketLeaveEarth: require("./gifs/Ship_TakeOff.gif"),
+  engineAnimation: require("./gifs/Ship_4_EngineAnimation.gif"),
+  shipExplode: require("./gifs/Ship_Explode.gif"),
+  approachJupiter: require("./gifs/Ship_ApproachJupiter.gif"),
+  approachMars: require("./gifs/Ship_ApproachMars.gif"),
+  approachMoon: require("./gifs/Ship_ApproachMoon.gif"),
+  approachNeptune: require("./gifs/Ship_ApproachNeptune.gif"),
+  approachPluto: require("./gifs/Ship_ApproachPluto.gif"),
+  approachSaturn: require("./gifs/Ship_ApproachSaturn.gif"),
+  approachUranus: require("./gifs/Ship_ApproachUranus.gif"),
+  approachVenus: require("./gifs/Ship_ApproachVenus.gif"),
+  marsSurface: require("./gifs/Ship_MarsSurface.png"),
+  mercuryLanding: require("./gifs/Ship_MercuryLanding.gif"),
+  moonLanding: require("./gifs/Ship_MoonLanding.gif"),
+  plutoSurface: require("./gifs/Ship_PlutoSurface.png")
+};
 
 // module.exports = {
 //     alt1: "Rocket ship!"
