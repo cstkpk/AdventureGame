d08e8501980ae5a993ec6daf3c0fe4dbd578497e
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-08-22 10:53:22 -0400

    Changed xs, span, and offset values in animation component so that everything displays evenly

--- client/src/components/Animation/index.js
+++ client/src/components/Animation/index.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { Card, Col } from 'react-bootstrap';
+import { Card, Col } from "react-bootstrap";
 // import Rocket from "./assets/rocket1.gif";
 
 function Animation(props) {
-
-    return (
-        <Col md={{span: 6, offset: 1}}>
-            <Card>
-                {/* <Card.Body> */}
-                    {/* <Card.Title className="text-center">This is our animation component! Yay Dan!</Card.Title> */}
-                    <Card.Img variant="bottom" src={props.image} alt={props.alt}/> 
-                {/* </Card.Body> */}
-            </Card>
-        </Col>
-    );
-};
+  return (
+    <Col xs={12} md={{ span: 4, offset: 2 }}>
+      <Card>
+        {/* <Card.Body> */}
+        {/* <Card.Title className="text-center">This is our animation component! Yay Dan!</Card.Title> */}
+        <Card.Img variant="bottom" src={props.image} alt={props.alt} />
+        {/* </Card.Body> */}
+      </Card>
+    </Col>
+  );
+}
 
 export default Animation;
--- client/src/components/SpaceAPI/index.js
+++ client/src/components/SpaceAPI/index.js
@@ -1,52 +1,58 @@
 import React from "react";
-import { Card, Col } from 'react-bootstrap';
+import { Card, Col } from "react-bootstrap";
 import PlanetDetail from "../PlanetDetail";
 import MoonDetail from "../MoonDetail";
 
-function SpaceAPI (props) {
-    if (props.englishName === "Earth") {
-        return (
-            <Col md={{span: 4}}>
-                <Card>
-                    <Card.Body>
-                        <Card.Title>Earth</Card.Title>
-                        <ul className="list-unstyled">
-                            <li><strong>Number of Moons: </strong>1</li>
-                            <li><strong>Gravity: </strong>9.8 m/s<sup>2</sup></li>
-                            <li><strong>Diameter: </strong>12,742 Kilometers</li>
-                        </ul>
-                    </Card.Body>
-                </Card>
-            </Col>
-        );
-    } else {
-        return (
-            <Col md={{span: 4}}>
-                <Card heading={props.englishName}>
-                    {props.isPlanet ? (
-                        <PlanetDetail
-                        englishName={props.englishName}
-                        moons={props.moons}
-                        discoveredBy={props.discoveredBy}
-                        discoveryDate={props.discoveryDate}
-                        gravity={props.gravity}
-                        radius={props.radius}
-                        />
-                    ) : (
-                        <MoonDetail
-                        englishName={props.englishName}
-                        // satelliteOf={props.aroundPlanet ? (around) : ("not a satellite")}
-                        satelliteOf="Earth"
-                        discoveredBy={props.discoveredBy}
-                        discoveryDate={props.discoveryDate}
-                        gravity={props.gravity}
-                        radius={props.radius}
-                        />
-                    )}
-                </Card>
-            </Col>
-        );
-    }
-};
+function SpaceAPI(props) {
+  if (props.englishName === "Earth") {
+    return (
+      <Col md={{ span: 4 }}>
+        <Card>
+          <Card.Body>
+            <Card.Title>Earth</Card.Title>
+            <ul className="list-unstyled">
+              <li>
+                <strong>Number of Moons: </strong>1
+              </li>
+              <li>
+                <strong>Gravity: </strong>9.8 m/s<sup>2</sup>
+              </li>
+              <li>
+                <strong>Diameter: </strong>12,742 Kilometers
+              </li>
+            </ul>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  } else {
+    return (
+      <Col md={{ span: 4 }}>
+        <Card heading={props.englishName}>
+          {props.isPlanet ? (
+            <PlanetDetail
+              englishName={props.englishName}
+              moons={props.moons}
+              discoveredBy={props.discoveredBy}
+              discoveryDate={props.discoveryDate}
+              gravity={props.gravity}
+              radius={props.radius}
+            />
+          ) : (
+            <MoonDetail
+              englishName={props.englishName}
+              // satelliteOf={props.aroundPlanet ? (around) : ("not a satellite")}
+              satelliteOf="Earth"
+              discoveredBy={props.discoveredBy}
+              discoveryDate={props.discoveryDate}
+              gravity={props.gravity}
+              radius={props.radius}
+            />
+          )}
+        </Card>
+      </Col>
+    );
+  }
+}
 
 export default SpaceAPI;
