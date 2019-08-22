74d4765bb127159fc3d34941c067d2f97bcb7aed
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-08-22 15:22:40 -0400

    Added styling for QA box.

--- client/src/components/QADisplay/index.js
+++ client/src/components/QADisplay/index.js
@@ -1,36 +1,34 @@
 import React from "react";
-import { Button, Card, Col } from 'react-bootstrap';
+import { Button, Card, Col } from "react-bootstrap";
+import "./style.css";
 
 function QADisplay(props) {
-    return (
-        <Col md={{span: 10, offset: 1}}>
-            <Card className="mt-4" text="white" bg="transparent">
-                <Card.Body>
-                    <Card.Text>
-                    {props.question.map(paragraphs => (
-                        <p
-                        key={paragraphs}
-                        >{paragraphs}</p>
-                    ))}
-                    </Card.Text>
-                    <Card.Text>
-                        {props.choices.map(choices => (
-                        <Button 
-                        variant="outline-light"
-                        className="btn-block"
-                        key={choices}
-                        val={choices}
-                        onClick={() => props.nextQuestion(choices)}
-                        >
-                        {choices}
-                        </Button>
-                        ))}
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        </Col>
-        
-    );
-};
+  return (
+    <Col md={{ span: 8, offset: 2 }}>
+      <Card className="mt-4" text="white" bg="transparent">
+        <Card.Body>
+          <Card.Text>
+            {props.question.map(paragraphs => (
+              <p key={paragraphs}>{paragraphs}</p>
+            ))}
+          </Card.Text>
+          <Card.Text>
+            {props.choices.map(choices => (
+              <Button
+                variant="outline-light"
+                className="btn-block"
+                key={choices}
+                val={choices}
+                onClick={() => props.nextQuestion(choices)}
+              >
+                {choices}
+              </Button>
+            ))}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+}
 
 export default QADisplay;
