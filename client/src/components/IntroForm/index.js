7aa070640a169529ed74272d74f3bece2b6d497f
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-08-22 13:37:22 -0400

    Added initial styling to intro form and modal. Updated pluto approach gif

--- client/src/components/IntroForm/index.js
+++ client/src/components/IntroForm/index.js
@@ -1,29 +1,27 @@
 import React from "react";
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form } from "react-bootstrap";
+import "./style.css";
 
-function IntroForm (props) {
-    return (
-        <div id={props.id}>
-            <Form>
-                <Form.Group>
-                    <Form.Label text="white">Enter your name:</Form.Label>
-                    <Form.Control 
-                    type="text" 
-                    placeholder="Your name"
-                    name={props.name}
-                    value={props.value}
-                    onChange={props.onChange}
-                    />
-                </Form.Group>
-            <Button 
-            type="submit" 
-            onClick={(event) => props.toggleHidden(event)}
-            >
-            Begin
-            </Button>
-            </Form>
-        </div>
-    );
-};
+function IntroForm(props) {
+  return (
+    <div className="container" id={props.id}>
+      <Form id="name-form">
+        <Form.Group>
+          <Form.Label text="white">Enter your name:</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Your name"
+            name={props.name}
+            value={props.value}
+            onChange={props.onChange}
+          />
+        </Form.Group>
+        <Button type="submit" onClick={event => props.toggleHidden(event)}>
+          Begin
+        </Button>
+      </Form>
+    </div>
+  );
+}
 
 export default IntroForm;
