70b64d288eb568a6fdd8d223bc27b3239e0bad0a
Cara Sunberg <cnsunberg@gmail.com>
2019-08-21 21:01:51 -0400

    Continuing to play with possibilities for results page

--- client/src/pages/Results.js
+++ client/src/pages/Results.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import API from "../utils/API";
-import { Container } from 'react-bootstrap';
+import { Container, Card } from 'react-bootstrap';
 import "./Home.css";
 
 class Results extends Component {
@@ -37,8 +37,14 @@ class Results extends Component {
                 <div id='stars'></div>
                 <div id='stars2'></div>
                 <div id='stars3'></div>
-                <p>This might be a results page?</p>
-                <p>{this.state.user.element1 ? "true" : "false"}</p>
+                <Card>
+                    <Card.Body>
+                        <Card.Text>
+                            <p>{this.state.user.element1 ? "true" : "false"}</p>
+                            <p>{this.state.user.box1 || this.state.user.box2 ? "YOU FOUND THE BOXES yeehaw" : "You found NOTHING."}</p>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
             </Container>
         );
     };
