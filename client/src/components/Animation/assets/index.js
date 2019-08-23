e3d55cc4d229fe8707c571ffeac8e6b5d0d045e0
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-08-23 12:58:10 -0400

    Updated intro GIF to expand top and bottom

--- client/src/components/Animation/assets/index.js
+++ client/src/components/Animation/assets/index.js
@@ -1,18 +1,67 @@
 module.exports = {
-  rocket1: {image: require("./rocket1.gif"), alt: "Rocket"},
-  rocketLeaveEarth: {image: require("./gifs/Ship_TakeOff.gif"), alt: "Rocket leaving Earth"},
-  engineAnimation: {image: require("./gifs/Ship_4_EngineAnimation.gif"), alt: "Engine animation"},
-  shipExplode: {image: require("./gifs/Ship_Explode.gif"), alt: "Rocket shit explodes"},
-  approachJupiter: {image: require("./gifs/Ship_ApproachJupiter.gif"), alt: "Rocket approaches Jupiter"},
-  approachMars: {image: require("./gifs/Ship_ApproachMars.gif"), alt: "Rocket approaches Mars"},
-  approachMoon: {image: require("./gifs/Ship_ApproachMoon.gif"), alt: "Rocket approaches the Moon"},
-  approachNeptune: {image: require("./gifs/Ship_ApproachNeptune.gif"), alt: "Rocket approaches Neptune"},
-  approachPluto: {image: require("./gifs/Ship_ApproachPluto.gif"), alt: "Rocket approaches Pluto"},
-  approachSaturn: {image: require("./gifs/Ship_ApproachSaturn.gif"), alt: "Rocket approaches Saturn"},
-  approachUranus: {image: require("./gifs/Ship_ApproachUranus.gif"), alt: "Rocket approaches Uranus"},
-  approachVenus: {image: require("./gifs/Ship_ApproachVenus.gif"), alt: "Rocket approaches Venus"},
-  marsSurface: {image: require("./gifs/Ship_MarsSurface.png"), alt: "Mars' surface"},
-  mercuryLanding: {image: require("./gifs/Ship_MercuryLanding.gif"), alt: "Rocket lands on Mercury"},
-  moonLanding: {image: require("./gifs/Ship_MoonLanding.gif"), alt: "Rocket lands on the Moon"},
-  plutoSurface: {image: require("./gifs/Ship_PlutoSurface.png"), alt: "Pluto's surface"}
+  bedroomAnimation: {
+    image: require("./gifs/intro_bedroom_wide.gif"),
+    alt: "Bedroom Scene"
+  },
+  rocket1: { image: require("./rocket1.gif"), alt: "Rocket" },
+  rocketLeaveEarth: {
+    image: require("./gifs/Ship_TakeOff.gif"),
+    alt: "Rocket leaving Earth"
+  },
+  engineAnimation: {
+    image: require("./gifs/Ship_4_EngineAnimation.gif"),
+    alt: "Engine animation"
+  },
+  shipExplode: {
+    image: require("./gifs/Ship_Explode.gif"),
+    alt: "Rocket shit explodes"
+  },
+  approachJupiter: {
+    image: require("./gifs/Ship_ApproachJupiter.gif"),
+    alt: "Rocket approaches Jupiter"
+  },
+  approachMars: {
+    image: require("./gifs/Ship_ApproachMars.gif"),
+    alt: "Rocket approaches Mars"
+  },
+  approachMoon: {
+    image: require("./gifs/Ship_ApproachMoon.gif"),
+    alt: "Rocket approaches the Moon"
+  },
+  approachNeptune: {
+    image: require("./gifs/Ship_ApproachNeptune.gif"),
+    alt: "Rocket approaches Neptune"
+  },
+  approachPluto: {
+    image: require("./gifs/Ship_ApproachPluto.gif"),
+    alt: "Rocket approaches Pluto"
+  },
+  approachSaturn: {
+    image: require("./gifs/Ship_ApproachSaturn.gif"),
+    alt: "Rocket approaches Saturn"
+  },
+  approachUranus: {
+    image: require("./gifs/Ship_ApproachUranus.gif"),
+    alt: "Rocket approaches Uranus"
+  },
+  approachVenus: {
+    image: require("./gifs/Ship_ApproachVenus.gif"),
+    alt: "Rocket approaches Venus"
+  },
+  marsSurface: {
+    image: require("./gifs/Ship_MarsSurface.png"),
+    alt: "Mars' surface"
+  },
+  mercuryLanding: {
+    image: require("./gifs/Ship_MercuryLanding.gif"),
+    alt: "Rocket lands on Mercury"
+  },
+  moonLanding: {
+    image: require("./gifs/Ship_MoonLanding.gif"),
+    alt: "Rocket lands on the Moon"
+  },
+  plutoSurface: {
+    image: require("./gifs/Ship_PlutoSurface.png"),
+    alt: "Pluto's surface"
+  }
 };
